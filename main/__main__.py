b2cad11c630bc6f921f8c9d1ec07ac061d85065f
impesx <impesx@gmail.com>
2024-06-20 23:13:07 +0800

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -4,18 +4,18 @@ from main.utils import load_plugins
 import logging
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import threading
-from . import bot
+# from . import bot
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
 path = "main/plugins/*.py"
 files = glob.glob(path)
-for name in files:
-    with open(name) as a:
-        patt = Path(a.name)
-        plugin_name = patt.stem
-        load_plugins(plugin_name.replace(".py", ""))
+# for name in files:
+#     with open(name) as a:
+#         patt = Path(a.name)
+#         plugin_name = patt.stem
+#         load_plugins(plugin_name.replace(".py", ""))
 
 # Don't be a thief
 print("Successfully deployed!")
